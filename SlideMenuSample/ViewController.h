c2a57f40b6e2ca3800bf913cebf748ddd6aaa31a
Rajagopal G <rajagopalg@rajagopals-mac-mini.local>
2017-01-11 10:47:32 +0530

    Initial Commit

---
+++ SlideMenuSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideMenuSample
+//
+//  Created by Rajagopal G on 11/01/17.
+//  Copyright © 2017 TringappsPvtLtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SlideMenuSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SlideMenuSample
+//
+//  Created by Rajagopal G on 11/01/17.
+//  Copyright © 2017 TringappsPvtLtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
